6b30aa2e59ba41ee60dca3722e77545a6015838e
GUNJI CHANIKYA <chanikyagunji7@gmail.com>
2026-02-02 23:32:03 +0530

    Create Main class with welcome messages

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Smart Task & Productivity Analyzer");
+        System.out.println("This app helps you manage tasks and time.");
+
+    }
+}
